a563fb8bed002a7c99aadaf650a24d5253de48aa
Wayou Liu <liuwayong@gmail.com>
2014-06-05 20:15:53 +0800

    add boilerplate files

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		// Import package manifest
+		pkg: grunt.file.readJSON("boilerplate.jquery.json"),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.licenses[0].type %> License\n" +
+				" */\n"
+		},
+
+		// Concat definitions
+		concat: {
+			dist: {
+				src: ["src/jquery.boilerplate.js"],
+				dest: "dist/jquery.boilerplate.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+
+		// Lint definitions
+		jshint: {
+			files: ["src/jquery.boilerplate.js"],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+
+		// Minify definitions
+		uglify: {
+			my_target: {
+				src: ["dist/jquery.boilerplate.js"],
+				dest: "dist/jquery.boilerplate.min.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+
+		// CoffeeScript compilation
+		coffee: {
+			compile: {
+				files: {
+					"dist/jquery.boilerplate.js": "src/jquery.boilerplate.coffee"
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-coffee");
+
+	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
+	grunt.registerTask("travis", ["jshint"]);
+
+};
