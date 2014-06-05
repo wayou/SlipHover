3b76a013da54b027fb0bb4ab6bfe2af7b7d3109b
Wayou <liuwayong@gmail.com>
2014-06-06 00:02:00 +0800

    add test page

--- src/jquery.sliphover.js
+++ src/jquery.sliphover.js
@@ -1,9 +1,10 @@
 /**
-*sliphover v1.2.0
-*https://github.com/wayou/SlipHover
-*/
+ *sliphover v1.2.0
+ *https://github.com/wayou/SlipHover
+ */
 
-;(function($, window, document, undefined) {
+;
+(function($, window, document, undefined) {
 
 
     // Create the defaults once
@@ -14,7 +15,7 @@
             duration: 'fast', //specify how long the animation will lasts in milliseconds
             fontColor: '#ffffff',
             backgroundColor: 'rgba(0,0,0,.5)', //specify the background color and opacity using rgba
-            reverse: false,//reverse the direction
+            reverse: false, //reverse the direction
             flip: false, //true to enable the 3d flip animation
             autoScroll: true, //whether auto scroll to show the caption when it is overflow
             scrollSpeed: 40, //if autoScroll is true, this option specify the scroll speed, the smaller the fast
@@ -33,15 +34,20 @@
 
     SlipHover.prototype = {
         init: function() {
-           
+
         },
-        yourOtherFunction: function() {
-            // some logic
+        getDirection: function($target, event) {
+            //reference: http://stackoverflow.com/questions/3627042/jquery-animation-for-a-hover-with-mouse-direction
+            //return a number with the maping 0 from top 1 from right 2 from bottom and 3 from the left
+            var w = $target.width(),
+                h = $target.height(),
+                x = (event.pageX - $target.offset().left - (w / 2)) * (w > h ? (h / w) : 1),
+                y = (event.pageY - $target.offset().top - (h / 2)) * (h > w ? (w / h) : 1),
+                direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4;
+            return direction;
         }
     };
 
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
     $.fn[pluginName] = function(options) {
         this.each(function() {
             if (!$.data(this, "plugin_" + pluginName)) {
