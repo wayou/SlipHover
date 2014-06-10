cc0671add7eece380dbfb904f2e14733efb86753
Wayou Liu <liuwayong@gmail.com>
2014-06-10 19:45:34 +0800

    remove flip option

--- src/jquery.sliphover.js
+++ src/jquery.sliphover.js
@@ -16,11 +16,10 @@
             caption: 'title', //the caption that will display when hover
             duration: 'fast', //specify how long the animation will lasts in milliseconds
             fontColor: '#fff',
+            textAlign:'center',
+            verticalAlign:'middle',
             backgroundColor: 'rgba(0,0,0,.5)', //specify the background color and opacity using rgba
             reverse: false, //reverse the direction
-            flip: false, //true to enable the 3d flip animation
-            autoScroll: true, //whether auto scroll to show the caption when it is overflow
-            scrollSpeed: 40, //if autoScroll is true, this option specify the scroll speed, the smaller the fast
             height: '100%' //specify the height of the overlay
         };
 
@@ -81,26 +80,33 @@
                 height: height,
                 top: top,
                 left: left,
+                display:'table',
+                position: 'absolute',
+                overflow: 'hidden'
             }).insertBefore($element);
 
             return $overlayContainer;
         },
         createOverlay: function(instance, direction, $element) {
 
-            var initClass, $overlay, content;
+            var left, bottom, $overlay, content;
 
             switch (direction) {
                 case 0: //from top
-                    initClass = 'sliphover-top';
+                    left = 0;
+                    bottom = '100%' ;
                     break;
                 case 1: //from right
-                    initClass = 'sliphover-right';
+                    left = '100%';
+                    bottom = 0;
                     break;
                 case 2: //from bottom
-                    initClass = 'sliphover-bottom';
+                    left = 0;
+                    bottom = '-100%' ;
                     break;
                 case 3: //from left
-                    initClass = 'sliphover-left';
+                    left = '-100%' ;
+                    bottom = 0;
                     break;
                 default:
                     window.console.error('error when get direction of the mouse');
@@ -108,10 +114,18 @@
 
             content = $element.prop(instance.settings.caption);
             $overlay = $('<div>', {
-                class: 'sliphover-overlay ' + initClass
+                class: 'sliphover-overlay'
             })
                 .css({
+                    position: 'relative',
+                    width: '100%',
+                    left: left,
+                    bottom: bottom,
+                    display:'table-cell',
+                    verticalAlign:instance.settings.verticalAlign,
+                    textAlign:instance.settings.textAlign,
                     height: instance.settings.height,
+                    color:instance.settings.fontColor,
                     backgroundColor: instance.settings.backgroundColor
                 })
                 .html(content);
